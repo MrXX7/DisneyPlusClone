7363a320da0213f723ff1ab8ba25daaafebdfe36
Oncu <oohancan@gmail.com>
2022-03-29 20:05:17 +0300

    Initial Commit

---
+++ DisneyPlusApp/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusAppApp.swift
+//  DisneyPlusApp
+//
+//  Created by Oncu Ohancan on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
