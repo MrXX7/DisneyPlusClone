fca6b81228e9b2846aa256e55c3b74b72fa13bea
Oncu <oohancan@gmail.com>
2022-04-03 04:49:01 +0300

    add model movie

---
+++ DisneyPlusApp/Utils/ColorConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ColorConstants.swift
+//  DisneyPlus
+//
+//  Created by Oncu Ohancan on 2.04.2022.
+//
+
+import SwiftUI
+
+struct ColorConstants {
+    static let darkGrayColor = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    static let darkBluishGrayColor = Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+}
+
+
