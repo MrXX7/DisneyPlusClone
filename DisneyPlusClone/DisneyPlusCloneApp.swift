59fcd27f14c29b224803850b15b810f258c2b44f
Oncu <oohancan@gmail.com>
2022-09-26 13:46:26 +0300

    Initial Commit

---
+++ DisneyPlusClone/DisneyPlusCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusCloneApp.swift
+//  DisneyPlusClone
+//
+//  Created by Oncu Can on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
