78472bcc47fa198c8cf52b21e7a862f1f0bde29a
Oncu <oohancan@gmail.com>
2022-09-26 14:21:17 +0300

    add SplashView

---
+++ DisneyPlusClone/Views/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  DisneyPlusClone
+//
+//  Created by Oncu Can on 26.09.2022.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
