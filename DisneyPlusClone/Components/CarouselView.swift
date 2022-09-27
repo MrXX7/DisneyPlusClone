dd6cc42fcf27cb75bfad81a7b8189ecc3557d841
Oncu <oohancan@gmail.com>
2022-09-27 15:52:35 +0300

    add ProfilePic and SquareButton

---
+++ DisneyPlusClone/Components/CarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  CarouselView.swift
+//  DisneyPlusClone
+//
+//  Created by Oncu Can on 27.09.2022.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarouselView()
+    }
+}
