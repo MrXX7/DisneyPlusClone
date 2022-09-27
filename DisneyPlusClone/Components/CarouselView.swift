f4aaef84f86c51f0d1dd06b50823f652e00a6a13
Oncu <oohancan@gmail.com>
2022-09-27 16:36:04 +0300

    add ScrollingHStackModifier and CarouselView

--- DisneyPlusClone/Components/CarouselView.swift
+++ DisneyPlusClone/Components/CarouselView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct CarouselView: View {
+    var promos: [Movie] = Movie.promos
+    @State private var selectedMovie: Movie?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center, spacing: 30) {
+            ForEach(0..<promos.count) { i in
+                ZStack(alignment: .bottom) {
+                    Image(promos[i].promoImage)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10)
+                    
+                    Button(action: {
+                        self.selectedMovie = promos[i]
+                    }) {
+                        Text(promos[i].title)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 5)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black.opacity(0.6).blur(radius: 3.0))
+                    }
+                    .sheet(item: self.$selectedMovie) { movie in
+                        
+                    }
+                }.frame(width: 400)
+            }
+        }.modifier(ScrollingHStackModifier(items: promos.count, itemWidth: UIScreen.main.bounds.width * 0.93, itemSpacing: 30))
     }
 }
 
